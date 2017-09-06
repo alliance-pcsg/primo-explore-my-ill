6caa6279f7528fb37bc509065f9fcdd3568ea658
Jeremy McWilliams <jeremym@lclark.edu>
2017-09-06 14:49:44 -0700

    updates take 2

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # primo-explore-my-ill
 Load illiad requests &amp; articles into Primo "My Account"
+
+
+# primo-explore-oadoi-link
+Provides link to Open Access content for articles, when available
+
+## Features
+In a full display record, checks for the presence of a DOI. Queries the [OADOI API (v2)](https://oadoi.org/api/v2), and upon the presence of an open access link, displays a bar with the link under the "View It" section.
+
+### Screenshot
+![screenshot](screenshot.png)
+
+## Install
+1. Make sure you've installed and configured [primo-explore-devenv](https://github.com/ExLibrisGroup/primo-explore-devenv).
+2. Navigate to your template/central package root directory. For example:
+    ```
+    cd primo-explore/custom/MY_VIEW_ID
+    ```
+3. If you do not already have a `package.json` file in this directory, create one:
+    ```
+    npm init -y
+    ```
+4. Install this package:
+    ```
+    npm install primo-explore-oadoi-link --save-dev
+    ```
+
+## Usage
+Once this package is installed, add `oadoi` as a dependency for your custom module definition.
+
+```js
+var app = angular.module('viewCustom', ['oadoi'])
+```
+
+Note: If you're using the `--browserify` build option, you will need to first import the module with:
+
+```javascript
+import 'primo-explore-oadoi-link';
+```
+
+You can configure the banner by passing a configuration object. All properties are required.
+
+| name      | type         | usage                                                                                   |
+|-----------|--------------|-----------------------------------------------------------------------------------------|
+| `imagePath` | string       | icon for next to the image link                                               |
+| `email` | string       | email address attached to api query                                               |
+
+The code below adds a banner similar to the above image.
+
+```js
+app.constant('oadoiOptions', {
+  "imagePath": "custom/LCC/img/oa_50.png",
+  "email": "youremail@domain.com"
+})
+```
+
+<!-- ## Running tests
+1. Clone the repo
+2. Run `npm install`
+3. Run `npm test` -->
--- php/illiad.php
+++ php/illiad.php
@@ -2,38 +2,33 @@
 
 
 //  $referer=$_SERVER["HTTP_REFERER"];
-  $domain=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+$domain=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
 
-  /* the whitelist array only allows requests from the included domains  */
-  $whitelist=array("primo.lclark.edu", "localhost","alliance-primo-sb.hosted.exlibrisgroup.com");
-  if (!in_array($domain, $whitelist)){exit();}
+/* the whitelist array only allows requests from the included domains  */
+$whitelist=array("primo.lclark.edu", "localhost","alliance-primo-sb.hosted.exlibrisgroup.com");
+if (!in_array($domain, $whitelist)){exit();}
 
-
-
-
-$key="e4a952ac-f82a-43ca-8cee-328270c7b044";
+/* ILLiad web platform key. For more info, check here: https://prometheus.atlas-sys.com/display/illiad/The+ILLiad+Web+Platform+API   */
+/* key should look something like this: 71ed0900-97aa-4157-ad9f-e74bcfa5e709*/
+$key="";    /* enter key here  */
 
 $data=array();
 $data["referer"]=$domain;
 $username=strtolower($_GET["user"]);
 
-
-
 $req="https://illiad.lclark.edu/ILLiadWebPlatform/Transaction/UserRequests/$username";
 
-
-
 $ch = curl_init();
-        // set url
+// set url
 curl_setopt($ch, CURLOPT_URL, $req);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/xml","ApiKey: $key"));
-        //return the transfer as a string
+//return the transfer as a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        // $output contains the output string
+// $output contains the output string
 $output = curl_exec($ch);
 
-        // close curl resource to free up system resources
+// close curl resource to free up system resources
 curl_close($ch);
 
 $xml=simplexml_load_string($output);
@@ -43,82 +38,82 @@ $art_c=1;
 
 foreach ($xml as $trans){
 
-	$status=$trans->TransactionStatus;
-  $id=$trans->TransactionNumber;
+$status=$trans->TransactionStatus;
+$id=$trans->TransactionNumber;
 
-  switch ($status){
+switch ($status){
 
-    case "Delivered to Web":
+case "Delivered to Web":
 
-    $cat="Articles";
-    $d=$trans->TransactionDate;
-    $date=convertDate($d);
-    $jtitle=$trans->PhotoJournalTitle;
-
-
-
-    $jvolume=$trans->PhotoJournalVolume;
-    $jissue=$trans->PhotoJournalIssue;
-    $year=$trans->PhotoJournalYear;
-    $author=$trans->PhotoArticleAuthor;
-    $title=$trans->PhotoArticleTitle[0];
+$cat="Articles";
+$d=$trans->TransactionDate;
+$date=convertDate($d);
+$jtitle=$trans->PhotoJournalTitle;
 
 
 
-    $url="https://illiad.lclark.edu/illiad/illiad.dll?Action=10&Form=75&Value=$id";
-
-    $data[$cat][$art_c]["id"]="$id";
-    $data[$cat][$art_c]["jtitle"]="$jtitle";
-    $data[$cat][$art_c]["jvolume"]="$jvolume";
-    $data[$cat][$art_c]["jissue"]="$jissue";
-    $data[$cat][$art_c]["year"]="$year";
-    $data[$cat][$art_c]["author"]="$author";
-    $data[$cat][$art_c]["title"]="$title";
-    $data[$cat][$art_c]["url"]="$url";
-    $data[$cat][$art_c]["count"]="$art_c";
-    $data[$cat][$art_c]["expires"]="$date";
-
-    $art_c++;
+$jvolume=$trans->PhotoJournalVolume;
+$jissue=$trans->PhotoJournalIssue;
+$year=$trans->PhotoJournalYear;
+$author=$trans->PhotoArticleAuthor;
+$title=$trans->PhotoArticleTitle[0];
 
 
-    break;
-    case "Request Sent":
 
-    $cat="Requests";
+$url="https://illiad.lclark.edu/illiad/illiad.dll?Action=10&Form=75&Value=$id";
 
-    $requestType=$trans->RequestType;
-    $loanAuthor=$trans->LoanAuthor;
-    $loanTitle=$trans->LoanTitle;
-    $articleTitle=$trans->PhotoArticleTitle;
-    $articleAuthor=$trans->PhotoArticleAuthor;
-    $documentType=$trans->DocumentType;
+$data[$cat][$art_c]["id"]="$id";
+$data[$cat][$art_c]["jtitle"]="$jtitle";
+$data[$cat][$art_c]["jvolume"]="$jvolume";
+$data[$cat][$art_c]["jissue"]="$jissue";
+$data[$cat][$art_c]["year"]="$year";
+$data[$cat][$art_c]["author"]="$author";
+$data[$cat][$art_c]["title"]="$title";
+$data[$cat][$art_c]["url"]="$url";
+$data[$cat][$art_c]["count"]="$art_c";
+$data[$cat][$art_c]["expires"]="$date";
 
-    $data[$cat][$req_c]["type"]="$requestType";
-    $data[$cat][$req_c]["docType"]="$documentType";
-    $data[$cat][$req_c]["count"]="$req_c";
-    if ($requestType=="Article"){
-      $data[$cat][$req_c]["author"]="$articleAuthor";
-      $data[$cat][$req_c]["title"]="$articleTitle";
+$art_c++;
 
 
-    }
-    if ($requestType=="Loan"){
-      $data[$cat][$req_c]["author"]="$loanAuthor";
-      $data[$cat][$req_c]["title"]="$loanTitle";
+break;
+case "Request Sent":
+
+$cat="Requests";
+
+$requestType=$trans->RequestType;
+$loanAuthor=$trans->LoanAuthor;
+$loanTitle=$trans->LoanTitle;
+$articleTitle=$trans->PhotoArticleTitle;
+$articleAuthor=$trans->PhotoArticleAuthor;
+$documentType=$trans->DocumentType;
+
+$data[$cat][$req_c]["type"]="$requestType";
+$data[$cat][$req_c]["docType"]="$documentType";
+$data[$cat][$req_c]["count"]="$req_c";
+if ($requestType=="Article"){
+$data[$cat][$req_c]["author"]="$articleAuthor";
+$data[$cat][$req_c]["title"]="$articleTitle";
 
 
-    }
-    $req_c++;
+}
+if ($requestType=="Loan"){
+$data[$cat][$req_c]["author"]="$loanAuthor";
+$data[$cat][$req_c]["title"]="$loanTitle";
 
 
-  //var_dump($transaction);
+}
+$req_c++;
 
 
-    break;
-    default:
-    //do nothing
+//var_dump($transaction);
 
-  }
+
+break;
+default:
+//do nothing
+
+}
 
 
 
@@ -134,13 +129,13 @@ echo json_encode($data);
 
 function convertDate($orig){
 
-  $pieces=explode("T", $orig);
-  array_pop($pieces);
-  $date=implode(" ", $pieces);
+$pieces=explode("T", $orig);
+array_pop($pieces);
+$date=implode(" ", $pieces);
 
-  $datetime = new DateTime($date);
-  $datetime->modify('+30 day');
-  return $datetime->format('M jS, Y');
+$datetime = new DateTime($date);
+$datetime->modify('+30 day');
+return $datetime->format('M jS, Y');
 
 
 }
