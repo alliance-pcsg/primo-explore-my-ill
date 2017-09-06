b43330de60e2789b46df8b9c4e48bf3f2fe2c164
Jeremy McWilliams <jeremym@lclark.edu>
2017-09-06 14:45:52 -0700

    some php/readme updates

---
+++ index.js
@@ -0,0 +1,2 @@
+require('./js/my-ill.module.js')
+module.exports = 'myILL'
--- php/illiad.php
+++ php/illiad.php
@@ -1,118 +1,140 @@
 <?php
 
-/* Edit these variables */
-$whitelist=array("primo.lclark.edu", "localhost"); /* whitelisted domains for requesting*/
-$illiadDomain=""; /* e.g "https://illiad.lclark.edu"*/
-$dsn = ""; // Data Source Name (DSN) from the file /usr/local/zend/etc/odbc.ini
-$user = ""; // MSSQL database user
-$password = ""; // MSSQL user password
-$server="";  /* e.g. illiad.lclark.edu */
-$database=""; /* e.g. ILLiadData*/
+
+//  $referer=$_SERVER["HTTP_REFERER"];
+  $domain=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+
+  /* the whitelist array only allows requests from the included domains  */
+  $whitelist=array("primo.lclark.edu", "localhost","alliance-primo-sb.hosted.exlibrisgroup.com");
+  if (!in_array($domain, $whitelist)){exit();}
 
 
-$referer=$_SERVER["HTTP_REFERER"];
-$domain=parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
 
-if (!in_array($domain, $whitelist)){exit();}
+
+$key="e4a952ac-f82a-43ca-8cee-328270c7b044";
 
 $data=array();
-$username=$_GET["user"];
-
-$dbhandle = mssql_connect($server, $user, $password)
-or die("Couldn't connect to SQL Server on $myServer");
-
-$selected = mssql_select_db($databse, $dbhandle)
-or die("Couldn't open database $database");
-
-
-/* Articles */
-
-$query="select TransactionNumber, PhotoJournalTitle, PhotoJournalVolume, PhotoJournalIssue, PhotoJournalYear, PhotoArticleAuthor, PhotoArticleTitle, TransactionDate from Transactions where Username='$username' and TransactionStatus='Delivered to Web'";
-
-//execute the SQL query and return records
-$result = mssql_query($query);
-$numRows = mssql_num_rows($result);
 $data["referer"]=$domain;
-$cat="Articles";
-$c=1;
-if ($numRows==0){$data[$cat]=false;}
-
-else{
+$username=strtolower($_GET["user"]);
 
 
-//display the results
-  while($row = mssql_fetch_array($result))
-  {
-    $id=$row["TransactionNumber"];
-    $jtitle=$row["PhotoJournalTitle"];
-    $jvolume=$row["PhotoJournalVolume"];
-    $jissue=$row["PhotoJournalIssue"];
-    $year=$row["PhotoJournalYear"];
-    $author=$row["PhotoArticleAuthor"];
-    $title=$row["PhotoArticleTitle"];
-    $url=$illiadDomain."/illiad/illiad.dll?Action=10&Form=75&Value=$id";
-    $d=$row["TransactionDate"];
+
+$req="https://illiad.lclark.edu/ILLiadWebPlatform/Transaction/UserRequests/$username";
+
+
+
+$ch = curl_init();
+        // set url
+curl_setopt($ch, CURLOPT_URL, $req);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/xml","ApiKey: $key"));
+        //return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string
+$output = curl_exec($ch);
+
+        // close curl resource to free up system resources
+curl_close($ch);
+
+$xml=simplexml_load_string($output);
+
+$req_c=1;
+$art_c=1;
+
+foreach ($xml as $trans){
+
+	$status=$trans->TransactionStatus;
+  $id=$trans->TransactionNumber;
+
+  switch ($status){
+
+    case "Delivered to Web":
+
+    $cat="Articles";
+    $d=$trans->TransactionDate;
     $date=convertDate($d);
+    $jtitle=$trans->PhotoJournalTitle;
 
-    $data[$cat][$c]["id"]=$id;
-    $data[$cat][$c]["jtitle"]=$jtitle;
-    $data[$cat][$c]["jvolume"]=$jvolume;
-    $data[$cat][$c]["jissue"]=$jissue;
-    $data[$cat][$c]["year"]=$year;
-    $data[$cat][$c]["author"]=$author;
-    $data[$cat][$c]["title"]=$title;
-    $data[$cat][$c]["url"]=$url;
-    $data[$cat][$c]["count"]=$c;
-    $data[$cat][$c]["expires"]=$date;
 
-    $c++;
 
-  }
-}
+    $jvolume=$trans->PhotoJournalVolume;
+    $jissue=$trans->PhotoJournalIssue;
+    $year=$trans->PhotoJournalYear;
+    $author=$trans->PhotoArticleAuthor;
+    $title=$trans->PhotoArticleTitle[0];
 
-/* Requests */
 
-$cat="Requests";
 
-$sql="select TransactionNumber, RequestType,LoanAuthor,LoanTitle,PhotoJournalTitle, PhotoJournalVolume,PhotoJournalIssue,PhotoJournalYear,PhotoArticleAuthor,PhotoArticleTitle,DocumentType from Transactions where Username='$username' and TransactionStatus='Request Sent'";
-//echo $sql;
-$result = mssql_query($sql);
-$numRows = mssql_num_rows($result);
-if ($numRows==0){$data[$cat]=false;}
+    $url="https://illiad.lclark.edu/illiad/illiad.dll?Action=10&Form=75&Value=$id";
 
-else{
-  $d=1;
-  while($row = mssql_fetch_array($result))
-  {
-    $requestType=$row["RequestType"];
-    $loanAuthor=$row["LoanAuthor"];
-    $loanTitle=$row["LoanTitle"];
-    $articleTitle=$row["PhotoArticleTitle"];
-    $articleAuthor=$row["PhotoArticleAuthor"];
-    $documentType=$row["DocumentType"];
+    $data[$cat][$art_c]["id"]="$id";
+    $data[$cat][$art_c]["jtitle"]="$jtitle";
+    $data[$cat][$art_c]["jvolume"]="$jvolume";
+    $data[$cat][$art_c]["jissue"]="$jissue";
+    $data[$cat][$art_c]["year"]="$year";
+    $data[$cat][$art_c]["author"]="$author";
+    $data[$cat][$art_c]["title"]="$title";
+    $data[$cat][$art_c]["url"]="$url";
+    $data[$cat][$art_c]["count"]="$art_c";
+    $data[$cat][$art_c]["expires"]="$date";
 
-    $data[$cat][$d]["type"]=$requestType;
-    $data[$cat][$d]["docType"]=$documentType;
-    $data[$cat][$d]["count"]=$d;
+    $art_c++;
+
+
+    break;
+    case "Request Sent":
+
+    $cat="Requests";
+
+    $requestType=$trans->RequestType;
+    $loanAuthor=$trans->LoanAuthor;
+    $loanTitle=$trans->LoanTitle;
+    $articleTitle=$trans->PhotoArticleTitle;
+    $articleAuthor=$trans->PhotoArticleAuthor;
+    $documentType=$trans->DocumentType;
+
+    $data[$cat][$req_c]["type"]="$requestType";
+    $data[$cat][$req_c]["docType"]="$documentType";
+    $data[$cat][$req_c]["count"]="$req_c";
     if ($requestType=="Article"){
-      $data[$cat][$d]["author"]=$articleAuthor;
-      $data[$cat][$d]["title"]=$articleTitle;
+      $data[$cat][$req_c]["author"]="$articleAuthor";
+      $data[$cat][$req_c]["title"]="$articleTitle";
 
 
     }
     if ($requestType=="Loan"){
-      $data[$cat][$d]["author"]=$loanAuthor;
-      $data[$cat][$d]["title"]=$loanTitle;
+      $data[$cat][$req_c]["author"]="$loanAuthor";
+      $data[$cat][$req_c]["title"]="$loanTitle";
+
+
     }
-    $d++;
+    $req_c++;
+
+
+  //var_dump($transaction);
+
+
+    break;
+    default:
+    //do nothing
+
   }
+
+
+
+
+
+
+
 }
+
+
 
 echo json_encode($data);
 
 function convertDate($orig){
 
-  $pieces=explode(" ", $orig);
+  $pieces=explode("T", $orig);
   array_pop($pieces);
   $date=implode(" ", $pieces);
 
@@ -125,5 +147,4 @@ function convertDate($orig){
 
 
 
-
- ?>
+?>
